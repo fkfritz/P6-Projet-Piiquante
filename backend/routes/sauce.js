b5a6dd67a0a7aea00cc91e91aac7cdeada9ccc93
Fritznel Franck <univfranck@gmail.com>
2021-10-28 10:50:06 -0300

    import du middleware d'authentification

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/sauce");
+const auth = require("../middleware/auth");
 
 router.get("/", auth, userCtrl.getSauce);
 router.get("/:id", auth, userCtrl.getOneSauce);
