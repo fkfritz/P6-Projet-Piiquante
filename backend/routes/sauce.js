327cc0bade008c2d95ae0f89e54a82f106c9e041
Fritznel Franck <univfranck@gmail.com>
2021-10-29 11:05:33 -0300

    Import de multer dans les routes sauce

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/sauce");
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 router.get("/", auth, userCtrl.getSauce);
 router.get("/:id", auth, userCtrl.getOneSauce);
-router.post("/", auth, userCtrl.createSauce);
-router.put("/:id", auth, userCtrl.modifySauce);
+router.post("/", auth, multer, userCtrl.createSauce);
+router.put("/:id", auth, multer, userCtrl.modifySauce);
 router.delete("/:id", auth, userCtrl.deleteSauce);
 
 module.exports = router;
